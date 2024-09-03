3538479665569eaad4a31dddeb59cc1d3bc00ad0
Viesturs-Vilnis Emerbergs <11DPVEmerbergs@rvt.lv>
2024-09-03 13:27:58 +0300

    Created base project template.

---
+++ Cicifus/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cicifus.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
