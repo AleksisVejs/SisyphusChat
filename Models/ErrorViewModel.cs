f99086778b0fd21043d25ef54b1f066547d32e55
Viesturs-Vilnis Emerbergs <11DPVEmerbergs@rvt.lv>
2024-09-10 12:02:44 +0300

    Removed old and incorrect project structure

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Sisyphus.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
