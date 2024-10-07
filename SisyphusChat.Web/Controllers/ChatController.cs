7fcb99a2b55ed4a1ad61399cc460e1853e2b8a84
22DP1ACiru <11dpacirulis@rvt.lv>
2024-10-07 14:19:29 +0300

    Added Chat controller and view.

---
+++ SisyphusChat.Web/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SisyphusChat.Web.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
