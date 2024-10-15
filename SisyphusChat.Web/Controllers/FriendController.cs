ebce203a8921f62dfce9eb2618ebe56bc6c1ece3
Gustavs-Jekabs Zeme <11DGZeme@rvt.lv>
2024-10-15 16:25:01 +0300

    gramming in progress

---
+++ SisyphusChat.Web/Controllers/FriendController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SisyphusChat.Web.Controllers
+{
+    public class FriendController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
