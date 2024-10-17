777cdb18a8df342a0aa63064c769ae1a6733ff65
AleksisVejs <11davejs@rvt.lv>
2024-10-17 13:29:43 +0300

    Updated dark/light theme and home page layout

--- SisyphusChat.Web/wwwroot/js/site.js
+++ SisyphusChat.Web/wwwroot/js/site.js
@@ -1,35 +1,34 @@
-﻿var themeSwitcher = document.getElementById("theme-switcher");
+﻿document.addEventListener("DOMContentLoaded", function() {
+  var themeSwitcher = document.getElementById("theme-switcher");
 
-// Load theme from local storage
-var savedTheme = localStorage.getItem("theme");
-if (savedTheme) {
-  setTheme(savedTheme);
-}
+  // Load theme from local storage
+  var savedTheme = localStorage.getItem("theme");
+  if (savedTheme) {
+      setTheme(savedTheme);
+  }
 
-themeSwitcher.addEventListener("click", function () {
-  var themeLink = document.getElementById("theme-link");
-  var moonIcon = document.getElementById("moon-icon");
+  themeSwitcher.addEventListener("click", function () {
+      var themeLink = document.getElementById("theme-link");
+      var moonIcon = document.getElementById("moon-icon");
 
-  if (themeLink.getAttribute("href") == "/css/light-theme.css") {
-    themeLink.setAttribute("href", "/css/dark-theme.css");
-    moonIcon.style.fill = "black";
-    localStorage.setItem("theme", "dark"); // Save theme to local storage
-  } else {
-    themeLink.setAttribute("href", "/css/light-theme.css");
-    moonIcon.style.fill = "white";
-    localStorage.setItem("theme", "light"); // Save theme to local storage
+      if (themeLink.getAttribute("href").includes("light-theme")) {
+          themeLink.setAttribute("href", "/css/dark-theme.css");
+          moonIcon.style.fill = "black";
+          localStorage.setItem("theme", "dark"); // Save theme to local storage
+      } else {
+          themeLink.setAttribute("href", "/css/light-theme.css");
+          moonIcon.style.fill = "white";
+          localStorage.setItem("theme", "light"); // Save theme to local storage
+      }
+  });
+
+  function setTheme(theme) {
+      var moonIcon = document.getElementById("moon-icon");
+
+      if (theme === "dark") {
+          moonIcon.style.fill = "black";
+      } else {
+          moonIcon.style.fill = "white";
+      }
   }
 });
-
-function setTheme(theme) {
-  var themeLink = document.getElementById("theme-link");
-  var moonIcon = document.getElementById("moon-icon");
-
-  if (theme === "dark") {
-    themeLink.setAttribute("href", "/css/dark-theme.css");
-    moonIcon.style.fill = "black";
-  } else {
-    themeLink.setAttribute("href", "/css/light-theme.css");
-    moonIcon.style.fill = "white";
-  }
-}
