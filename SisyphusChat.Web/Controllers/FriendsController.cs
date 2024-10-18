603dc37e05dcd072d9162fd7ebf386914a968367
21DPGZeme <11dgzeme@rvt.lv>
2024-10-18 11:42:05 +0300

    Merge with main

--- SisyphusChat.Web/Controllers/FriendsController.cs
+++ SisyphusChat.Web/Controllers/FriendsController.cs
@@ -1,17 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SisyphusChat.Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using SisyphusChat.Infrastructure.Entities;
+using SisyphusChat.Core.Services;
+using SisyphusChat.Web.Models;
 
 namespace SisyphusChat.Web.Controllers
 {
     public class FriendsController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             return View();
         }
 
-        public IActionResult AddFriends()
+        public async Task<IActionResult> AddFriends()
         {
             return View();
         }
+
+        // public async Task<IActionResult> SendRequest(string receiverid){}
+
+        // public async Task<IActionResult> CancelRequest(string receiverid){}
+
+        // public async Task<IActionResult> AcceptRequest(){}
+
+        // public async Task<IActionResult> DenyRequest(){}
+        
+        // public async Task<IActionResult> RemoveFriend(friendid){}
     }
 }
