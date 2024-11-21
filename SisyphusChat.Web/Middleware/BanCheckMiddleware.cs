cfce29b94b0725cbfbebaed42d5b5e972657716d
Viesturs Vilnis Emerbergs <103423179+22DPVEmer@users.noreply.github.com>
2024-11-21 14:29:28 +0200

    Clean up in some parts, some new comments.

--- SisyphusChat.Web/Middleware/BanCheckMiddleware.cs
+++ SisyphusChat.Web/Middleware/BanCheckMiddleware.cs
@@ -17,18 +17,19 @@ namespace SisyphusChat.Web.Middleware
         public async Task InvokeAsync(HttpContext context, UserManager<User> userManager)
         {
             if (context.Request.Path.StartsWithSegments("/Identity/Account/Logout") ||
-               // context.Request.Path.StartsWithSegments("/Home/Error") ||
-                context.Request.Path.StartsWithSegments("/notificationHub"))
+                context.Request.Path.StartsWithSegments("/Home/Error")
+              )
             {
                 await _next(context);
                 return;
             }
-
+            // to check if user is authenticated
             if (context.User?.Identity?.IsAuthenticated == true)
             {
                 var user = await userManager.GetUserAsync(context.User);
                 if (user != null && user.IsBanned)
                 {
+                    // to check if user is not already on the banned page
                     if (!context.Request.Path.StartsWithSegments("/Home/Banned") && 
                         !context.Request.Path.StartsWithSegments("/Home/PermanentlyBanned"))
                     {
