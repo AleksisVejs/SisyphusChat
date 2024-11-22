bd2153ba71ab9a3a4039cdb320c38263d623cc09
Viesturs Vilnis Emerbergs <103423179+22DPVEmer@users.noreply.github.com>
2024-11-22 09:12:50 +0200

    Fixed attachment services

--- SisyphusChat.Core/Interfaces/IAttachmentService.cs
+++ SisyphusChat.Core/Interfaces/IAttachmentService.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using SisyphusChat.Core.Interfaces;
 using SisyphusChat.Infrastructure.Entities;
+using SisyphusChat.Core.Models;
 
 namespace SisyphusChat.Core.Interfaces
 {
-    public interface IAttachmentService : ICrud<Attachment>
+    public interface IAttachmentService : ICrud<AttachmentModel>
     {
-        Task<Attachment> GetByIdAsync(string id);
-        Task<ICollection<Attachment>> GetAllByMessageIdAsync(string messageId);
+        Task<AttachmentModel> GetByIdAsync(string id);
+        Task<ICollection<AttachmentModel>> GetAllByMessageIdAsync(string messageId);
     }
 }
